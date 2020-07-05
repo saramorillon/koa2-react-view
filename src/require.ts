440c91ad94eafdb96e263fc3f1acd769fdb63bdd
Sara Morillon <sara.morillon@gmail.com>
2020-07-05 12:26:36 +0200

    Add babel register to require jsx files

--- src/require.ts
+++ src/require.ts
@@ -1,3 +1,12 @@
+import pluginTransform from '@babel/plugin-transform-runtime'
+import presetEnv from '@babel/preset-env'
+import presetReact from '@babel/preset-react'
+
+require('@babel/register')({
+  presets: [presetReact, presetEnv],
+  plugins: [pluginTransform],
+})
+
 export function _cleanCache(): void {
   Object.keys(require.cache).forEach(function (key) {
     delete require.cache[key]
---
+++ src/typings.d.ts
@@ -0,0 +1,3 @@
+declare module '@babel/preset-react'
+declare module '@babel/preset-env'
+declare module '@babel/plugin-transform-runtime'
